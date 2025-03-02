d261e19cd96e00d28a7646444b53371a9780e241
FAHAD kabir <hmdipto7@gmail.com>
2025-03-01 21:25:30 -0500

    'gitIgnore'

--- coffe-store-server/index.js
+++ coffe-store-server/index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -9,14 +10,10 @@ app.use(cors());
 app.use(express.json())
 
 //MongoDB Connection
-// coffeStore
-//coffE521
 
 
-
-// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.tljeb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri = "mongodb+srv://coffeStore:coffE521@cluster0.tljeb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tljeb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
